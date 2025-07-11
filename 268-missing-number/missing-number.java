77488930217b75a8bc0df0a8640b96a8292bbc07
Naik-Nagaprasad <nagaprasadnaikcode@gmail.com>
2025-07-11 22:42:11 +0530

    Time: 8 ms (5.73%) | Memory: 45.4 MB (43.23%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], 1);
+        }
+        int res = 0;
+        for(int i=0; i<=nums.length; i++){
+            if(map.getOrDefault(i, 0) == 0){
+                res = i;
+            }
+        }
+        return res;
+    }
+}
