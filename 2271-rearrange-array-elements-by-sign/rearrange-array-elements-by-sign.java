e5656106e9ec7b92fa251f6e59e1b64d449d19b6
Naik-Nagaprasad <nagaprasadnaikcode@gmail.com>
2025-07-13 23:44:09 +0530

    Time: 3 ms (100.00%) | Memory: 81.7 MB (21.26%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,22 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        int[] even = new int[n/2];
-        int[] odd = new int[n/2];
-        int e=0, o=0;
+        //brute force
+        // int[] even = new int[n/2];
+        // int[] odd = new int[n/2];
+        // int e=0, o=0;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]>=0)
+        //         even[e++] = nums[i];
+        //     else
+        //         odd[o++] = nums[i];
+        // }
+        // e=0; o=0;
+        // for(int i=0; i<n; i++){
+        //     if( i%2 == 0)
+        //         nums[i] = even[e++];
+        //     else
+        //         nums[i] = odd[o++];
+        // }
+
+        //small change
+        int[] arr = new int[n];
+        int p=0, neg=1;
         for(int i=0; i<n; i++){
-            if(nums[i]>=0)
-                even[e++] = nums[i];
-            else
-                odd[o++] = nums[i];
+            if(nums[i]>=0){
+                arr[p] = nums[i];
+                p += 2;
+            }else{
+                arr[neg] = nums[i];
+                neg += 2;
+            }
         }
-        e=0; o=0;
-        for(int i=0; i<n; i++){
-            if( i%2 == 0)
-                nums[i] = even[e++];
-            else
-                nums[i] = odd[o++];
-        }
-        return nums;
+        return arr;
     }
 }
