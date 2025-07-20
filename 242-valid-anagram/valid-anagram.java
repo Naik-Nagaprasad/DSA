6d0c854bbcefbf79c6846033fed87430920c1972
Naik-Nagaprasad <nagaprasadnaikcode@gmail.com>
2025-07-20 11:14:33 +0530

    Time: 5 ms (56.48%) | Memory: 43.1 MB (81.83%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[256];
+        if(s.length() != t.length())
+            return false;
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i)]++;
+            arr[t.charAt(i)]--;
+        }
+        for(int i=0; i<256; i++){
+            if(arr[i]>0)
+                return false;
+        }
+        return true;
+    }
+}
