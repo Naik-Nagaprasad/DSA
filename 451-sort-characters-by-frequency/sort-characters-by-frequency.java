b2c3bad127b970287964274a49d026657b67c156
Naik-Nagaprasad <nagaprasadnaikcode@gmail.com>
2025-09-02 17:44:02 +0530

    Time: 25 ms (13.58%) | Memory: 44.8 MB (96.15%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,37 +1,66 @@
-class Element implements Comparable<Element>{
-    char ch;
-    int freq;
+// class Element implements Comparable<Element>{
+//     char ch;
+//     int freq;
 
-    public Element(char ch, int freq){
-        this.ch = ch;
-        this.freq = freq;
-    }
+//     public Element(char ch, int freq){
+//         this.ch = ch;
+//         this.freq = freq;
+//     }
 
-    public int compareTo(Element that){
-        return that.freq - this.freq;
-    }
-}
+//     public int compareTo(Element that){
+//         return that.freq - this.freq;
+//     }
+// }
+
+// class Solution {
+//     public String frequencySort(String s) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for(char ch : s.toCharArray()){
+//             map.put(ch, map.getOrDefault(ch, 0)+1);
+//         }
+//         PriorityQueue<Element> pq = new PriorityQueue<>();
+//         for(Map.Entry<Character, Integer> entry : map.entrySet()){
+//             Element element = new Element(entry.getKey(), entry.getValue());
+//             pq.offer(element);
+//         }
+
+//         StringBuilder str = new StringBuilder();
+//         while(pq.size()>0){
+//             Element ele = pq.poll();
+//             while(ele.freq>0){
+//                 str.append(ele.ch);
+//                 ele.freq--;
+//             }
+//         }
+//         return str.toString();
+//     }
+// }
+
+
 
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        PriorityQueue<Element> pq = new PriorityQueue<>();
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-            Element element = new Element(entry.getKey(), entry.getValue());
-            pq.offer(element);
+        int[] freq = new int[256];
+
+        for(int i=0; i<s.length(); i++){
+            freq[s.charAt(i)]++;
         }
 
-        StringBuilder str = new StringBuilder();
-        while(pq.size()>0){
-            Element ele = pq.poll();
-            while(ele.freq>0){
-                str.append(ele.ch);
-                ele.freq--;
-            }
+        Character[] arr = new Character[s.length()];
+        for(int i=0; i<s.length(); i++){
+            arr[i] = s.charAt(i);
         }
-        return str.toString();
+
+        Arrays.sort(arr, (a, b)->{
+            if(freq[b] == freq[a])
+                return a-b;
+            return freq[b] - freq[a];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(char c: arr){
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
